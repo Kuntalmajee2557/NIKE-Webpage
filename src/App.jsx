fc4eb7f6a7ee1345db8ae3a2ffd361b5f2274d2c
gitcoder <kuntalmajee338@gmail.com>
2024-03-10 00:19:48 +0530

    complete project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navigation from './componants/navigation';
+import HeroSection from './componants/hero';
+const App = () => {
+  return (
+  <div className='container'>
+    <Navigation/>
+    <HeroSection/>
+
+  </div>)
+};
+
+export default App;
---
+++ src/componants/navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation =() => {
+    return(
+        <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png"></img>
+        </div>
+  
+        <ul>
+          <li herf="#">Menu</li>
+          <li herf="#">Location</li>
+          <li herf="#">About</li>
+          <li herf="#">Contact</li>
+  
+        </ul>
+        <button>login</button>
+      </nav>
+    );
+};
+
+export default Navigation;
